a770a335e4f331a8da55c91234942dc3ae7335db
DaoKhanhDuy <info.daokhanhduy@gmail.com>
2022-09-26 17:42:36 +0000

    first commit

---
+++ utils/log.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+
+module.exports = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.yellow('[Lỗi] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.red('[Lỗi] » ') + data);
+			break;
+		default:
+			console.log(chalk.magenta(`${option} » `) + data);
+			break;
+	}
+}
+
+module.exports.loader = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.yellow('[Nino Cute] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.red('[Nino Cute] » ') + data);
+			break;
+		default:
+			console.log(chalk.green(`[Nino Cute] » `) + data);
+			break;
+	}
+}
